ec34fe14fd6373d47abc15d84a4c0266705aa13b
Jason Van Malder <jason.vanmalder@haulogy.net>
2024-11-10 16:50:21 +0100

    feat: add pg host in .env

--- src/adapters/db.ts
+++ src/adapters/db.ts
@@ -2,7 +2,7 @@ import { Client } from 'pg';
 
 const client = new Client({
   user: process.env.POSTGRES_USER,
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
